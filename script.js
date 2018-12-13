647d518c607380cd4a317cbcfaef7c95d14b5b6e
PawelTulicki <pawel.tulicki@onet.pl>
2018-12-13 14:23:06 +0100

    First

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function () {
+    $(":file").change(function () {
+        if (this.files && this.files[0]) {
+            var reader = new FileReader();
+            reader.onload = imageIsLoaded;
+            reader.readAsDataURL(this.files[0]);
+        }
+    });
+});
+
+function imageIsLoaded(e) {
+    $('#myImg').attr('src', e.target.result);
+    $('#myImg').addClass('col-sm-6 col-md-3 col-lg-3 col-xl-3 img-fluid');
+};
